d1ed605160ed67f3c9f26f728c89c4830221a137
Debayan2 <debayanbiswal7@gmail.com>
2025-03-17 11:34:37 +0530

    Updated test with new changes

--- tests/test_bff.py
+++ tests/test_bff.py
@@ -4,3 +4,6 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app.main import app  # Now pytest can find the 'app' module
+
+def test_sample():
+    assert 1 + 1 == 2  # Dummy test
